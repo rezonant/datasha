95624f0ee451caac842bc1d8fb0f215b41ba6d37
Liam (GH:rezonant) <wilahti@gmail.com>
2015-09-19 00:07:12 -0400

    Obey config.js

--- web/src/datasha.js
+++ web/src/datasha.js
@@ -1,4 +1,8 @@
 
+if (!window.appConfig) {
+	window.appConfig = require('../config.js');
+}
+
 var ngm = angular.module(module.exports = 'datasha', [
 	require('./datasha/ui'),
 	require('./datasha/api'),
